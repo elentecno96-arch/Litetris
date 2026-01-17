348a9908d28a8c5695e4491fae77970357146c26
elentenco96 <elentecno96@gmail.com>
2026-01-17 13:53:41 +0900

    UI 애니메이션 로직 분리 및 UIPanelEffect 컴포넌트 도입
    1. UIManager 내부에 직접 구현되어 있던 DOTween 애니메이션 로직을 UIPanelEffect 컴포넌트로 변경

---
+++ Assets/Game/Script/Component/UIPanel/UIPanelEffect.cs
@@ -0,0 +1,36 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Script.Component.UIPanel
+{
+    public class UIPanelEffect : MonoBehaviour
+    {
+        private float openDuration = 0.4f;
+        private float closeDuration = 0.3f;
+        //Ease는 보통 Easing을 의미하며 값이 시간에 따라 어떻게 변화되는지 정의하는 함수
+        //얼마나 빠르게 시작하고, 중간에 어떻게 변화하며, 끝날 때 어떻게 끝나는지 결정
+        private Ease openEase = Ease.OutBack;
+        private Ease closeEase = Ease.InBack;
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            transform.DOKill(); //트윈 중지
+            transform.localScale = Vector3.zero; //초기 설정
+            transform.DOScale(Vector3.one, openDuration)
+                     .SetEase(openEase)
+                     .SetUpdate(true); //시간과 무관하게 재생
+        }
+        public void Hide()
+        {
+            transform.DOKill();
+
+            transform.DOScale(Vector3.zero, closeDuration)
+                     .SetEase(closeEase)
+                     .SetUpdate(true)
+                     .OnComplete(() => gameObject.SetActive(false));
+        }
+    }
+}
